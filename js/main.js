fa26df7088c9b14c1af72b74cdd0ca9e63f4a357
1uanss <sousasluan212@gmail.com>
2026-03-08 09:30:18 -0300

    Final do js do projeto

--- js/main.js
+++ js/main.js
@@ -1,21 +1,57 @@
 const pokemonName = document.querySelector('.pokemon__name')
 const pokemonNumber = document.querySelector('.pokemon__number')
 const pokemonImage = document.querySelector('.pokemon__img')
+const form = document.querySelector('.form')
+const inputSearch = document.querySelector('.input__search')
+const btnPrev = document.querySelector('.btn-prev')
+const btnNext = document.querySelector('.btn-next')
+
+let searchPokemon = 1
 const fetchPokemon = async (pokemon) => {
-
-    
 const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-const data = await APIResponse.json()
 
-return data
+if(APIResponse.status === 200){
+    const data = await APIResponse.json()
+    return data
+}
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...'
+    pokemonImage.style.display = 'nome'
+    pokemonNumber.innerHTML = ''
     const data = await fetchPokemon(pokemon)
-
-    pokemonName.innerHTML = data.name
-    pokemonNumber.innerHTML = data.id
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-    
+    if(data){
+        pokemonName.innerHTML = data.name
+        pokemonNumber.innerHTML = data.id
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        inputSearch.value = ''
+        searchPokemon = data.id
+    }else{
+        pokemonName.innerHTML = 'Not Found'
+        pokemonNumber.innerHTML = ''
+        pokemonImage.style.display = 'nome'
+    }
 }
-renderPokemon('22')
+
+addEventListener('submit', (event)=>{
+event.preventDefault()
+renderPokemon(inputSearch.value.toLowerCase())
+
+})
+
+btnPrev.addEventListener('click', ()=>{
+    if(searchPokemon > 1){
+        searchPokemon -= 1
+        renderPokemon(searchPokemon)
+    }
+
+})
+
+btnNext.addEventListener('click', ()=>{
+searchPokemon += 1
+renderPokemon(searchPokemon)
+})
+
+
+renderPokemon(searchPokemon)
